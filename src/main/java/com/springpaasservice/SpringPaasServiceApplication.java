308f0ec0d6f35ee7f8a78543fd087c9a779c6c5f
Alberto <albarrio85@gmail.com>
2021-04-15 21:01:00 +0200

    initialize application

---
+++ src/main/java/com/springpaasservice/SpringPaasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springpaasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaasServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springpaasservice/SpringPaasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpaasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
