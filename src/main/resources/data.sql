f363733d180ce6488d0d820ff680ac9013f9427a
Alberto <albarrio85@gmail.com>
2021-04-17 20:26:31 +0200

    restore insert data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into quote_entity
+        (id, author, description, create_timestamp, update_timestamp)
+    values
+        (1000,'Elon Musk', 'Life is too short for long-term grudges', CURRENT_TIMESTAMP ,CURRENT_TIMESTAMP);
